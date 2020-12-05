f599b1ce4d09b9eebf5db6dee561abc6dbeb48a4
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-04 19:50:11 -0500

    included util packages and setting up connection.query

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,21 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const util = require("util");
 
 var connection = mysql.createConnection({
     host: "localhost",
     // Your port; if not 3306
     port: 3306,
-
     // Your username
     user: "root",
-
     // Your password
     password: "password",
     database: "employee_db"
 });
 
-connection.connect( function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
 
 module.exports = connection;
