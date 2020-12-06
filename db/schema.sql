51d8194afcef3f469894efbd571cfaa0feb1f6be
Anjali Kevadiya <angeldungrani5@gmail.com>
2020-12-05 21:46:39 -0500

    change data in seeds.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,9 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10, 2) NOT NULL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    FOREIGN KEY (department_id) 
+        REFERENCES department(id) 
+        ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -23,6 +25,10 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    FOREIGN KEY (role_id) 
+        REFERENCES role(id) 
+        ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) 
+        REFERENCES employee(id) 
+        ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,31 @@
-INSERT INTO department (name) VALUES ('IT');
-INSERT INTO department (name) VALUES ('Finance');
-INSERT INTO department (name) VALUES ('Marketing');
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
 
-INSERT INTO role (title, salary, department_id) VALUES ('Manager', 110000.00, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Engineer', 90000.00, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Manager', 110000.00, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('Accountant', 80000.00, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('Manager', 110000.00, 3);
-INSERT INTO role (title, salary, department_id) VALUES ('Sales', 60000.00, 3);
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Joe', 'Han', 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Manoli', 'K', 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('Angie', 'Chilmaza', 5);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Anjali', 'Kevadiya',  2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sean', 'Allen', 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Ketty', 'Paul', 6, 5);
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doe', 1, NULL),
+    ('Mike', 'Chan', 2, 1),
+    ('Ashley', 'Rodriguez', 3, NULL),
+    ('Kevin', 'Tupik', 4, 3),
+    ('Kunal', 'Singh', 5, NULL),
+    ('Malia', 'Brown', 6, 5),
+    ('Sarah', 'Lourd', 7, NULL),
+    ('Tom', 'Allen', 8, 7);
